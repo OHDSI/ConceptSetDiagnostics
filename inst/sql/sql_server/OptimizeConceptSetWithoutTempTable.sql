d9695eb52e348a40a1ae059b27059023b8e69d5f
Gowtham Rao <gowthamrao@gmail.com>
2021-03-02 08:40:45 -0500

    Updated sql logic for optimize concept set

--- inst/sql/sql_server/OptimizeConceptSetWithoutTempTable.sql
+++ inst/sql/sql_server/OptimizeConceptSetWithoutTempTable.sql
@@ -8,7 +8,7 @@ AS (
 	-- Concepts that are part of the concept set definition that are "EXCLUDED = N, DECENDANTS = Y or N"
 	SELECT DISTINCT concept_id,
 		ISNULL(standard_concept,'N') as standard_concept,
-		invalid_reason
+		ISNULL(invalid_reason,'V') as invalid_reason
 	FROM @vocabulary_database_schema.concept
 	WHERE concept_id IN (@conceptSetConceptIdsNotExcluded)
 	),
@@ -28,7 +28,7 @@ AS (
 	FROM @vocabulary_database_schema.concept_relationship cr
 	INNER JOIN conceptSetConceptsNotExcluded ON concept_id = concept_id_1
 		AND relationship_id = 'Maps to'
-	WHERE standard_concept = 'S'
+	WHERE ISNULL(standard_concept,'N') = 'S'
 		AND cr.invalid_reason IS NULL
 	), 
 conceptSetConceptsExcluded
@@ -36,7 +36,7 @@ AS (
 	-- Concepts that are part of the concept set definition that are "EXCLUDED = Y, DECENDANTS = Y or N"
 	SELECT DISTINCT concept_id,
 		ISNULL(standard_concept,'N') as standard_concept,
-		invalid_reason
+		ISNULL(invalid_reason,'V') as invalid_reason
 	FROM @vocabulary_database_schema.concept
 	WHERE concept_id IN (@conceptSetConceptIdsExcluded)
 	),
