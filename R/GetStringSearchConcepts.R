117f7dfe459b7669f906ab293c3a3e844cd5e6af
Gowtham Rao <gowthamrao@gmail.com>
2021-03-05 18:14:09 -0500

    Changed the string search to not use wildcard in short words plus optimization

--- R/GetStringSearchConcepts.R
+++ R/GetStringSearchConcepts.R
@@ -6,29 +6,42 @@ getStringSearchConcepts <-
            searchString,
            fullTextSearch = FALSE,
            vocabularyDatabaseSchema = 'vocabulary') {
+    # Note this function is designed for postgres with TSV enabled.
     # Filtering strings to letters, numbers and spaces only to avoid SQL injection
     # also making search string of lower case - to make search uniform.
     searchString <-
-      tolower(gsub("[^a-zA-Z0-9 ,]", " ", searchString))
+      stringr::str_squish(tolower(gsub("[^a-zA-Z0-9 ,]", " ", searchString)))
+    
+    # reversing for reverse search in TSV
     searchStringReverse <- stringi::stri_reverse(str = searchString)
+    ## if any string is shorter than 5 letters than it brings back
+    ## non specific search result
+    searchStringReverse <- stringr::str_split(string = searchStringReverse, pattern = " ") %>% unlist()
+    for (i in (1:length(searchStringReverse))) {
+      if (nchar(searchStringReverse[[i]]) < 5) {
+        searchStringReverse[[i]] <- ''
+      }
+    }
+    searchStringReverse <- stringr::str_squish(paste(searchStringReverse, collapse = " ")) 
     
-    if (length(unlist(strsplit(searchString, " "))) > 1) {
-      searchStringTsv <-
-        paste(paste0(unlist(x = strsplit(
-          x = searchString, split = " "
-        )), ':*'), collapse = " & ")
-    } else {
-      searchStringTsv <- paste0(searchString, ':*')
+    # function to create TSV string for post gres
+    stringForTsvSearch <- function(string) {
+      string <- stringr::str_squish(string)
+      # split the string to vector
+      stringSplit = strsplit(
+        x = string, split = " "
+      ) %>% unlist()
+      # add wild card only if word is atleast three characters long
+      for (i in (1:length(stringSplit))) {
+        if (nchar(stringSplit[[i]]) > 2) {
+          stringSplit[[i]] <- paste0(stringSplit[[i]], ':*')
+        }
+      }
+      return(paste(stringSplit, collapse = " & "))
     }
     
-    if (length(unlist(strsplit(searchStringReverse, " "))) > 1) {
-      searchStringReverseTsv <-
-        paste(paste0(unlist(x = strsplit(
-          x = searchStringReverse, split = " "
-        )), ':*'), collapse = " & ")
-    } else {
-      searchStringReverseTsv <- paste0(searchStringReverse, ':*')
-    }
+    searchStringTsv <- stringForTsvSearch(searchString)
+    searchStringReverseTsv <- stringForTsvSearch(searchStringReverse)
     
     pathToSql <- system.file("sql/sql_server", 
                              "SearchVocabularyForConcepts.sql", 
@@ -50,15 +63,6 @@ getStringSearchConcepts <-
         snakeCaseToCamelCase = TRUE
       ) %>%
       dplyr::tibble()
-    
-    conceptSetExpressionFromConceptTable <-
-      getConceptSetExpressionFromConceptTable(conceptTable = data,
-                                              selectAllDescendants = TRUE)
-    if (length(conceptSetExpressionFromConceptTable) > 0) {
-      conceptSetExpressionTable <- 
-        getConceptSetDataFrameFromExpression(conceptSetExpression = conceptSetExpressionFromConceptTable) %>% 
-        dplyr::left_join(y = data, by = 'conceptId')
-    }
 
-    return(conceptSetExpressionTable)
+    return(data)
   }
