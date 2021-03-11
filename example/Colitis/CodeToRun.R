794af24ec4c7d7961e20ae2f1e48067a2102a53d
Gowtham Rao <gowthamrao@gmail.com>
2021-03-10 19:04:24 -0500

    Updated examples

---
+++ example/Colitis/CodeToRun.R
@@ -0,0 +1,109 @@
+keyWords1 <- 'Colitis'
+
+folder <- stringr::str_replace_all(string = keyWords1,
+                                   pattern = " ",
+                                   replacement = "")
+# Load the package
+library(ConceptSetDiagnostics)
+
+locationForResults <-
+  file.path(rstudioapi::getActiveProject(), 'example', folder)
+dir.create(path = locationForResults,
+           recursive = TRUE,
+           showWarnings = FALSE)
+
+# Details for connecting to the server:
+connectionDetails <-
+  DatabaseConnector::createConnectionDetails(
+    dbms = "postgresql",
+    server = paste(
+      Sys.getenv("phenotypeLibraryDbServer"),
+      Sys.getenv("phenotypeLibraryDbDatabase"),
+      sep = "/"
+    ),
+    user = Sys.getenv("shinyDbUserGowtham"),
+    password = Sys.getenv("shinyDbPasswordGowtham"),
+    port = Sys.getenv("phenotypeLibraryDbPort")
+  )
+
+connection <-
+  DatabaseConnector::connect(connectionDetails = connectionDetails)
+
+# get search results
+conceptSetExpressionTable1 <-
+  ConceptSetDiagnostics::getStringSearchConcepts(connection = connection,
+                                                 searchString = keyWords1)
+conceptSetExpressionTable <- conceptSetExpressionTable1
+
+##  do automated processing based on search results
+designDiagnostic <-
+  performDesignDiagnosticsOnConceptTable(
+    connection = connection,
+    conceptSetExpressionTable = conceptSetExpressionTable,
+    exportResults = TRUE,
+    locationForResults = locationForResults,
+    iteration = 1
+  )
+
+
+
+
+############### review only standard for round 1
+discoveredThruRecommender <- c(77025, 4342751, 
+                               4187875,	
+                               4322739,	
+                               4298577,
+                               4326827,
+                               46273469,
+                               4201583,
+                               4179201,
+                               4093449,
+                               4171367)
+# conceptId	conceptName	vocabularyId	domainId	standardConcept	conceptInSet	rc	dc	drc	dbc
+# 77025	Diverticulitis of colon	SNOMED	Condition	S	Not included - descendant	22135417	15	22155542	16
+# 4342751	Sigmoiditis	SNOMED	Condition	S	Not included - descendant	252	2	667686	19
+# 4187875	Proctosigmoiditis	SNOMED	Condition	S	Not included - descendant	3544	2	652187	18
+# 4322739	Diverticulitis of sigmoid colon	SNOMED	Condition	S	Not included - descendant	14080	4	14847	4
+# 4298577	Diverticulitis of colon with perforation	SNOMED	Condition	S	Not included - descendant	3545	3	3969	3
+# 4326827	Diverticulitis of rectosigmoid	SNOMED	Condition	S	Not included - descendant	1733	2	1733	2
+# 46273469	Perforation of sigmoid colon due to diverticulitis	SNOMED	Condition	S	Not included - descendant	424	1	424	1
+# 4201583	Noninfectious sigmoiditis	SNOMED	Condition	S	Not included - descendant	200	2	400	2
+# 4179201	Perisigmoiditis	SNOMED	Condition	S	Not included - descendant	100	1	100	1
+# 4093449	Allergic sigmoiditis	SNOMED	Condition	S	Not included - descendant	100	1	100	1
+# 4171367	Dietetic sigmoiditis	SNOMED	Condition	S	Not included - descendant	100	1	100	1
+
+
+
+
+
+recommendedConceptSetExpression <- ConceptSetDiagnostics::getConceptIdDetails(connection = connection, 
+                                                                              conceptIds = discoveredThruRecommender)
+recommendedConceptSetExpression <- recommendedConceptSetExpression %>% 
+  ConceptSetDiagnostics::getConceptSetExpressionFromConceptTable(selectAllDescendants = TRUE) %>% 
+  ConceptSetDiagnostics::getConceptSetDataFrameFromExpression() %>% 
+  dplyr::left_join(y = recommendedConceptSetExpression, by = 'conceptId')
+conceptSetExpressionTable <- dplyr::union(conceptSetExpressionTable, recommendedConceptSetExpression)
+blackList <- setdiff(designDiagnostic$recommendedStandard$conceptId, discoveredThruRecommender)
+designDiagnostic <-
+  performDesignDiagnosticsOnConceptTable(
+    connection = connection,
+    conceptSetExpressionTable = conceptSetExpressionTable,
+    exportResults = TRUE,
+    locationForResults = locationForResults,
+    blackList = blackList,
+    iteration = 2
+  )
+############## complete standard
+
+## no source codes recommended
+############## complete non standard
+
+
+readr::write_excel_csv(
+  x = designDiagnostic$conceptSetExpressionTableOptimized,
+  file = file.path(locationForResults, "finalConceptSetExpression.csv"),
+  append = FALSE,
+  na = ""
+) 
+
+DatabaseConnector::disconnect(connection = connection)
---
+++ example/Diarrhea/CodeToRun.R
@@ -0,0 +1,95 @@
+keyWords1 <- 'Diarrhea'
+
+folder <- stringr::str_replace_all(string = keyWords1,
+                                   pattern = " ",
+                                   replacement = "")
+# Load the package
+library(ConceptSetDiagnostics)
+
+locationForResults <-
+  file.path(rstudioapi::getActiveProject(), 'example', folder)
+dir.create(path = locationForResults,
+           recursive = TRUE,
+           showWarnings = FALSE)
+
+# Details for connecting to the server:
+connectionDetails <-
+  DatabaseConnector::createConnectionDetails(
+    dbms = "postgresql",
+    server = paste(
+      Sys.getenv("phenotypeLibraryDbServer"),
+      Sys.getenv("phenotypeLibraryDbDatabase"),
+      sep = "/"
+    ),
+    user = Sys.getenv("shinyDbUserGowtham"),
+    password = Sys.getenv("shinyDbPasswordGowtham"),
+    port = Sys.getenv("phenotypeLibraryDbPort")
+  )
+
+connection <-
+  DatabaseConnector::connect(connectionDetails = connectionDetails)
+
+# get search results
+conceptSetExpressionTable1 <-
+  ConceptSetDiagnostics::getStringSearchConcepts(connection = connection,
+                                                 searchString = keyWords1)
+conceptSetExpressionTable <- conceptSetExpressionTable1
+
+##  do automated processing based on search results
+designDiagnostic <-
+  performDesignDiagnosticsOnConceptTable(
+    connection = connection,
+    conceptSetExpressionTable = conceptSetExpressionTable,
+    exportResults = TRUE,
+    locationForResults = locationForResults,
+    iteration = 1
+  )
+
+
+
+
+############### review only standard for round 1
+discoveredThruRecommender <- c(4193870,4166768, 201780, 4302676,4085622,4032877,198671,196623)
+# 4193870	Bacterial dysentery	SNOMED	Condition	S	Not included - descendant	200	2	171813	20
+# 4166768	Bacillary dysentery	SNOMED	Condition	S	Not included - descendant	200	2	171613	20
+# conceptId	conceptName	vocabularyId	domainId	standardConcept	conceptInSet	rc	dc	drc	dbc
+# 4302676	Amebic dysentery	SNOMED	Condition	S	Not included - descendant	300	3	56975	18
+# 4085622	Acute amebic dysentery	SNOMED	Condition	S	Not included - descendant	14850	17	31967	18
+# 4032877	Chronic amebiasis	SNOMED	Condition	S	Not included - descendant	340	3	24708	14
+# 198671	Chronic intestinal amebiasis without abscess	SNOMED	Condition	S	Not included - descendant	24368	13	24368	13
+# 196623	Acute amebic dysentery without abscess	SNOMED	Condition	S	Not included - descendant	17117	13	17117	13
+
+
+
+
+recommendedConceptSetExpression <- ConceptSetDiagnostics::getConceptIdDetails(connection = connection,
+                                                                              conceptIds = discoveredThruRecommender)
+recommendedConceptSetExpression <- recommendedConceptSetExpression %>%
+  ConceptSetDiagnostics::getConceptSetExpressionFromConceptTable(selectAllDescendants = TRUE) %>%
+  ConceptSetDiagnostics::getConceptSetDataFrameFromExpression() %>%
+  dplyr::left_join(y = recommendedConceptSetExpression, by = 'conceptId')
+conceptSetExpressionTable <- dplyr::union(conceptSetExpressionTable, recommendedConceptSetExpression)
+blackList <- setdiff(designDiagnostic$recommendedStandard$conceptId, discoveredThruRecommender)
+designDiagnostic <-
+  performDesignDiagnosticsOnConceptTable(
+    connection = connection,
+    conceptSetExpressionTable = conceptSetExpressionTable,
+    exportResults = TRUE,
+    locationForResults = locationForResults,
+    blackList = blackList,
+    iteration = 2
+  )
+############## complete standard
+
+## no source codes recommended
+############## complete non standard
+
+
+readr::write_excel_csv(
+  x = designDiagnostic$conceptSetExpressionTableOptimized,
+  file = file.path(locationForResults, "finalConceptSetExpression.csv"),
+  append = FALSE,
+  na = ""
+) 
+
+DatabaseConnector::disconnect(connection = connection)
