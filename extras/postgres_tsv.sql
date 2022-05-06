a1a26e034aeaf329a1a555cee20887b41c2a0915
Gowtham Rao <rao@ohdsi.org>
2022-05-06 10:30:28 -0400

    add new vocabulary version

--- extras/postgres_tsv.sql
+++ extras/postgres_tsv.sql
@@ -4,33 +4,45 @@ CREATE EXTENSION pg_trgm;
 CREATE EXTENSION btree_gin;
 
 ALTER TABLE concept 
+	DROP COLUMN IF EXISTS CONCEPT_NAME_TSV;	
+	
+ALTER TABLE concept 
 	ADD COLUMN CONCEPT_NAME_TSV tsvector
-    	GENERATED ALWAYS AS (to_tsvector('english', concept_name)) STORED;
-CREATE INDEX concept_name_gin_idx ON concept USING GIN (concept_name);
+    	GENERATED ALWAYS AS (to_tsvector('english', LOWER(concept_name))) STORED;
 
 ALTER TABLE concept 
+	DROP COLUMN IF EXISTS CONCEPT_CODE_TSV;
+ALTER TABLE concept 
 	ADD COLUMN CONCEPT_CODE_TSV tsvector
-    	GENERATED ALWAYS AS (to_tsvector('english', concept_code)) STORED;
-CREATE INDEX concept_code_gin_idx ON concept USING GIN (concept_code);
+    	GENERATED ALWAYS AS (to_tsvector('english', LOWER(concept_code))) STORED;
 
+ALTER TABLE concept 
+	DROP COLUMN IF EXISTS CONCEPT_ID_TSV;
 ALTER TABLE concept 
 	ADD COLUMN CONCEPT_ID_TSV tsvector
     	GENERATED ALWAYS AS (to_tsvector('english', "concept_id"::text)) STORED;
-CREATE INDEX concept_id_gin_idx ON concept USING GIN (concept_id);
 
 ALTER TABLE concept 
 	ADD COLUMN CONCEPT_NAME_REVERSE_TSV tsvector
-    	GENERATED ALWAYS AS (to_tsvector('english', reverse(concept_name))) STORED;
-CREATE INDEX concept_name_reverse_gin_idx ON concept USING GIN (reverse(concept_name));
+    	GENERATED ALWAYS AS (to_tsvector('english', reverse(LOWER(concept_name)))) STORED;
 
 
-
+ALTER TABLE concept_synonym 
+	DROP COLUMN IF EXISTS concept_synonym_name_TSV;
 ALTER TABLE concept_synonym
 	ADD COLUMN concept_synonym_name_TSV tsvector
-    	GENERATED ALWAYS AS (to_tsvector('english', concept_synonym_name)) STORED;
-CREATE INDEX concept_synonym_name_gin_idx ON concept_synonym USING GIN (concept_synonym_name);
-
+    	GENERATED ALWAYS AS (to_tsvector('english', LOWER(concept_synonym_name))) STORED;
+ALTER TABLE concept_synonym 
+	DROP COLUMN IF EXISTS concept_synonym_name_REVERSE_TSV;
 ALTER TABLE concept_synonym
 	ADD COLUMN concept_synonym_name_REVERSE_TSV tsvector
-    	GENERATED ALWAYS AS (to_tsvector('english', reverse(concept_synonym_name))) STORED;
-CREATE INDEX concept_synonym_name_REVERSE_gin_idx ON concept_synonym USING GIN (reverse(concept_synonym_name));
+    	GENERATED ALWAYS AS (to_tsvector('english', reverse(LOWER(concept_synonym_name)))) STORED;
+		
+		
+		
+CREATE INDEX concept_name_gin_idx ON concept USING GIN (LOWER(concept_name));
+CREATE INDEX concept_code_gin_idx ON concept USING GIN (LOWER(concept_code));
+CREATE INDEX concept_id_gin_idx ON concept USING GIN (concept_id);
+CREATE INDEX concept_name_reverse_gin_idx ON concept USING GIN (reverse(LOWER(concept_name)));
+CREATE INDEX concept_synonym_name_gin_idx ON concept_synonym USING GIN (LOWER(concept_synonym_name));
+CREATE INDEX concept_synonym_name_REVERSE_gin_idx ON concept_synonym USING GIN (reverse(LOWER(concept_synonym_name)));
--- inst/sql/sql_server/SearchVocabularyForConceptsPostGresTsv.sql
+++ inst/sql/sql_server/SearchVocabularyForConceptsPostGresTsv.sql
@@ -21,12 +21,15 @@ SELECT c.CONCEPT_ID,
 	ISNULL(c.INVALID_REASON, 'V') INVALID_REASON,
 	c.CONCEPT_CODE,
 	c.CONCEPT_CLASS_ID,
-	c.DOMAIN_ID,
+	c.DOMAIN_ID 
+{@concept_prevalence_table != '' } ? {,
 	ISNULL(universe.RC, 0) RC,
 	ISNULL(universe.DBC, 0) DBC,
 	ISNULL(universe.DRC, 0) DRC,
-	ISNULL(universe.DDBC, 0) DDBC
+	ISNULL(universe.DDBC, 0) DDBC }
 FROM @vocabulary_database_schema.concept c
-INNER JOIN matched_concepts ON c.concept_id = matched_concepts.concept_id
-LEFT JOIN @concept_prevalence_table universe ON c.concept_id = universe.concept_id
-ORDER BY ISNULL(universe.DRC, 0) DESC;
+INNER JOIN matched_concepts
+	ON c.concept_id = matched_concepts.concept_id {@concept_prevalence_table != '' } ? {
+LEFT JOIN @concept_prevalence_table universe
+	ON c.concept_id = universe.concept_id}
+{@concept_prevalence_table != '' } ? {ORDER BY ISNULL(universe.DRC, 0) DESC};
