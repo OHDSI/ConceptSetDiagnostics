b0297d335fb7a7e592ae06d81cb190961b0959c0
Gowtham Rao <rao@ohdsi.org>
2022-07-21 16:40:51 -0400

    ds

--- man-roxygen/ConceptPrevalenceTable.R
+++
@@ -1,4 +0,0 @@
-#' @param conceptPrevalenceTable A reference table (can be schemaName.tableName) that
-#'                               holds the concept prevalence data. The required fields are
-#'                               concept_id, rc, drc, dbc, ddbc. In case of error,
-#'                               cocneptPrevalence is silently ignored.
