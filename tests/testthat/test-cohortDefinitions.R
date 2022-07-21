4c28d0004a990938cff236cdc0f2d64344be083a
Gowtham Rao <rao@ohdsi.org>
2022-07-21 13:49:52 -0400

    Package maintenance

--- tests/testthat/test-cohortDefinitions.R
+++ tests/testthat/test-cohortDefinitions.R
@@ -5,122 +5,152 @@ testthat::test_that("extractConceptSetsInCohortDefinition", {
   testthat::expect_null(object = suppressWarnings(
     extractConceptSetsInCohortDefinition(cohortExpression = cohortsExpressionNoConceptSet)
   ))
-  
+
   conceptSetsInCohort <-
     extractConceptSetsInCohortDefinition(cohortExpression = cohortExpression)
-  testthat::expect_gte(object = nrow(conceptSetsInCohort),
-                       expected = 0)
-  
+  testthat::expect_gte(
+    object = nrow(conceptSetsInCohort),
+    expected = 0
+  )
+
   a <- list()
   a$expression <- cohortExpression
   conceptSetsInCohort2 <-
     extractConceptSetsInCohortDefinition(cohortExpression = a)
-  testthat::expect_gte(object = nrow(conceptSetsInCohort2),
-                       expected = 0)
+  testthat::expect_gte(
+    object = nrow(conceptSetsInCohort2),
+    expected = 0
+  )
 })
 
 testthat::test_that("Check if cohort definition set", {
   conceptSetsInCohort <-
     extractConceptSetsInCohortDefinitionSet(cohortDefinitionSet = cohortDefinitionSet)
-  testthat::expect_gte(object = nrow(conceptSetsInCohort),
-                       expected = 0)
+  testthat::expect_gte(
+    object = nrow(conceptSetsInCohort),
+    expected = 0
+  )
 })
 
 testthat::test_that("optimizeConceptSetExpression", {
   conceptSetsInCohort <-
     extractConceptSetsInCohortDefinition(cohortExpression = cohortExpression)
-  
+
   conceptSetExpression1 <-
-    conceptSetsInCohort[1,]$conceptSetExpression %>%
+    conceptSetsInCohort[1, ]$conceptSetExpression %>%
     RJSONIO::fromJSON(digits = 23)
-  
+
   conceptSetsInCohort1 <-
     optimizeConceptSetExpression(
       conceptSetExpression = conceptSetExpression1,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connectionDetails = connectionDetails
     )
-  testthat::expect_gte(object = length(conceptSetsInCohort1),
-                       expected = 0)
-  testthat::expect_gte(object = length(conceptSetsInCohort1$recommended),
-                       expected = 0)
+  testthat::expect_gte(
+    object = length(conceptSetsInCohort1),
+    expected = 0
+  )
+  testthat::expect_gte(
+    object = length(conceptSetsInCohort1$recommended),
+    expected = 0
+  )
   testthat::expect_gte(
     object = nrow(conceptSetsInCohort1$recommendedAsDataFrame),
     expected = 0
   )
-  testthat::expect_gte(object = nrow(conceptSetsInCohort1$removed),
-                       expected = 0)
-  
+  testthat::expect_gte(
+    object = nrow(conceptSetsInCohort1$removed),
+    expected = 0
+  )
+
   conceptSetsInCohort2 <-
     optimizeConceptSetExpression(
       conceptSetExpression = conceptSetExpression1,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connection = DatabaseConnector::connect(connectionDetails)
     )
-  testthat::expect_gte(object = length(conceptSetsInCohort2),
-                       expected = 0)
-  testthat::expect_gte(object = length(conceptSetsInCohort2$recommended),
-                       expected = 0)
-  testthat::expect_gte(object = length(conceptSetsInCohort2$removed),
-                       expected = 0)
-  
-  
+  testthat::expect_gte(
+    object = length(conceptSetsInCohort2),
+    expected = 0
+  )
+  testthat::expect_gte(
+    object = length(conceptSetsInCohort2$recommended),
+    expected = 0
+  )
+  testthat::expect_gte(
+    object = length(conceptSetsInCohort2$removed),
+    expected = 0
+  )
+
+
   conceptSetExpression3 <-
-    conceptSetsInCohort[2,]$conceptSetExpression %>%
+    conceptSetsInCohort[2, ]$conceptSetExpression %>%
     RJSONIO::fromJSON(digits = 23)
-  
+
   conceptSetsInCohort1 <-
     optimizeConceptSetExpression(
       conceptSetExpression = conceptSetExpression3,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connectionDetails = connectionDetails
     )
-  testthat::expect_gte(object = length(conceptSetsInCohort1),
-                       expected = 0)
-  testthat::expect_gte(object = length(conceptSetsInCohort1$recommended),
-                       expected = 0)
-  testthat::expect_gte(object = length(conceptSetsInCohort1$removed),
-                       expected = 0)
+  testthat::expect_gte(
+    object = length(conceptSetsInCohort1),
+    expected = 0
+  )
+  testthat::expect_gte(
+    object = length(conceptSetsInCohort1$recommended),
+    expected = 0
+  )
+  testthat::expect_gte(
+    object = length(conceptSetsInCohort1$removed),
+    expected = 0
+  )
 })
 
 testthat::test_that("resolveConceptSetExpression", {
   conceptSetsInCohort <-
     extractConceptSetsInCohortDefinition(cohortExpression = cohortExpression)
-  
+
   conceptSetExpression1 <-
-    conceptSetsInCohort[1,]$conceptSetExpression %>%
+    conceptSetsInCohort[1, ]$conceptSetExpression %>%
     RJSONIO::fromJSON(digits = 23)
-  
+
   resolvedConceptSet1 <-
     resolveConceptSetExpression(
       conceptSetExpression = conceptSetExpression1,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connectionDetails = connectionDetails
     )
-  testthat::expect_gte(object = nrow(resolvedConceptSet1),
-                       expected = 0)
-  
+  testthat::expect_gte(
+    object = nrow(resolvedConceptSet1),
+    expected = 0
+  )
+
   conceptSetExpression2 <-
-    conceptSetsInCohort[2,]$conceptSetExpression %>%
+    conceptSetsInCohort[2, ]$conceptSetExpression %>%
     RJSONIO::fromJSON(digits = 23)
-  
+
   resolvedConceptSet3 <-
     resolveConceptSetExpression(
       conceptSetExpression = conceptSetExpression2,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connectionDetails = connectionDetails
     )
-  testthat::expect_gte(object = nrow(resolvedConceptSet3),
-                       expected = 0)
-  
+  testthat::expect_gte(
+    object = nrow(resolvedConceptSet3),
+    expected = 0
+  )
+
   resolvedConceptSet2 <-
     resolveConceptSetExpression(
       conceptSetExpression = conceptSetExpression1,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connection = DatabaseConnector::connect(connectionDetails)
     )
-  testthat::expect_gte(object = nrow(resolvedConceptSet2),
-                       expected = 0)
+  testthat::expect_gte(
+    object = nrow(resolvedConceptSet2),
+    expected = 0
+  )
 })
 
 
@@ -131,42 +161,50 @@ testthat::test_that("resolveConceptSetExpression", {
       connectionDetails = connectionDetail,
       vocabularyDatabaseSchema = cdmDatabaseSchemas
     )
-  testthat::expect_gte(object = nrow(resolvedConceptSet),
-                       expected = 0)
-  
+  testthat::expect_gte(
+    object = nrow(resolvedConceptSet),
+    expected = 0
+  )
+
   resolvedConceptSet2 <-
     resolveConceptSetsInCohortExpression(
       cohortExpression = cohortExpression,
       connection = DatabaseConnector::connect(connectionDetails = connectionDetail),
       vocabularyDatabaseSchema = cdmDatabaseSchemas
     )
-  testthat::expect_gte(object = nrow(resolvedConceptSet2),
-                       expected = 0)
+  testthat::expect_gte(
+    object = nrow(resolvedConceptSet2),
+    expected = 0
+  )
 })
 
 testthat::test_that("getExcludedConceptsInConceptSetExpression", {
   conceptSetsInCohort <-
     extractConceptSetsInCohortDefinition(cohortExpression = cohortExpression)
-  
+
   conceptSetExpressionWithExclusion <-
-    conceptSetsInCohort[3,]$conceptSetExpression %>%
+    conceptSetsInCohort[3, ]$conceptSetExpression %>%
     RJSONIO::fromJSON(digits = 23)
-  
+
   excludedConcepts1 <-
     getExcludedConceptsInConceptSetExpression(
-      cohortExpression = conceptSetExpressionWithExclusion,
-      connectionDetails = connectionDetail,
-      vocabularyDatabaseSchema = cdmDatabaseSchemas
+      conceptSetExpression = conceptSetExpressionWithExclusion,
+      connectionDetails = connectionDetails,
+      vocabularyDatabaseSchema = cdmDatabaseSchema
     )
-  testthat::expect_gte(object = nrow(excludedConcepts),
-                       expected = 0)
-  
+  testthat::expect_gte(
+    object = nrow(excludedConcepts),
+    expected = 0
+  )
+
   excludedConcepts2 <-
     getExcludedConceptsInConceptSetExpression(
-      cohortExpression = conceptSetExpressionWithExclusion,
-      connection = DatabaseConnector::connect(connectionDetails = connectionDetail),
-      vocabularyDatabaseSchema = cdmDatabaseSchemas
+      conceptSetExpression = conceptSetExpressionWithExclusion,
+      connection = DatabaseConnector::connect(connectionDetails = connectionDetails),
+      vocabularyDatabaseSchema = cdmDatabaseSchema
     )
-  testthat::expect_gte(object = nrow(excludedConcepts2),
-                       expected = 0)
+  testthat::expect_gte(
+    object = nrow(excludedConcepts2),
+    expected = 0
+  )
 })
