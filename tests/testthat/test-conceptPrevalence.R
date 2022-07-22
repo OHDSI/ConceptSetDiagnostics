d1766be9943860f034e0d4101bf9da639e10abba
Gowtham Rao <rao@ohdsi.org>
2022-07-22 11:49:51 -0400

    tests for conceptPrevalence (#23)

--- tests/testthat/test-conceptPrevalence.R
+++ tests/testthat/test-conceptPrevalence.R
@@ -2,40 +2,34 @@ testthat::test_that("Concept Prevalence - connection", {
   if (dbms == "postgresql") {
     connection <-
       DatabaseConnector::connect(connectionDetails = connectionDetails)
-
+    
     conceptPrevalence <- getConceptPrevalenceCounts(
       conceptIds = 0,
       connection = connection,
       conceptPrevalenceSchema = "concept_prevalence"
     )
-
-    testthat::expect_gte(
-      object = nrow(conceptPrevalence),
-      expected = 0
-    )
-
+    
+    testthat::expect_gte(object = nrow(conceptPrevalence),
+                         expected = 0)
+    
     recommendedStandard <- getRecommendedStandard(
       conceptIds = 0,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connection = connection,
       conceptPrevalenceSchema = "concept_prevalence"
     )
-    testthat::expect_gte(
-      object = nrow(recommendedStandard),
-      expected = 0
-    )
-
+    testthat::expect_gte(object = nrow(recommendedStandard),
+                         expected = 0)
+    
     recommendedSource <- getRecommendedSource(
       conceptIds = 0,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connection = connection,
       conceptPrevalenceSchema = "concept_prevalence"
     )
-    testthat::expect_gte(
-      object = nrow(recommendedSource),
-      expected = 0
-    )
-
+    testthat::expect_gte(object = nrow(recommendedSource),
+                         expected = 0)
+    
     DatabaseConnector::disconnect(connection = connection)
   }
 })
@@ -48,32 +42,62 @@ testthat::test_that("Concept Prevalence - connectionDetails", {
       connectionDetails = connectionDetails,
       conceptPrevalenceSchema = "concept_prevalence"
     )
-
-    testthat::expect_gte(
-      object = nrow(conceptPrevalence),
-      expected = 0
-    )
-
+    
+    testthat::expect_gte(object = nrow(conceptPrevalence),
+                         expected = 0)
+    
     recommendedStandard <- getRecommendedStandard(
       conceptIds = 0,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connectionDetails = connectionDetails,
       conceptPrevalenceSchema = "concept_prevalence"
     )
-    testthat::expect_gte(
-      object = nrow(recommendedStandard),
-      expected = 0
-    )
-
+    testthat::expect_gte(object = nrow(recommendedStandard),
+                         expected = 0)
+    
     recommendedSource <- getRecommendedSource(
       conceptIds = 0,
       vocabularyDatabaseSchema = cdmDatabaseSchema,
       connectionDetails = connectionDetails,
       conceptPrevalenceSchema = "concept_prevalence"
     )
-    testthat::expect_gte(
-      object = nrow(recommendedSource),
-      expected = 0
+    testthat::expect_gte(object = nrow(recommendedSource),
+                         expected = 0)
+  }
+})
+
+
+testthat::test_that("Concept Prevalence - table does not exist", {
+  if (dbms == "postgresql") {
+    connection <-
+      DatabaseConnector::connect(connectionDetails = connectionDetails)
+    
+    testthat::expect_error(
+      getConceptPrevalenceCounts(
+        conceptIds = 0,
+        connection = connection,
+        conceptPrevalenceSchema = cdmDatabaseSchema
+      )
     )
+    
+    testthat::expect_error(
+      getRecommendedStandard(
+        conceptIds = 0,
+        vocabularyDatabaseSchema = cdmDatabaseSchema,
+        connection = connection,
+        conceptPrevalenceSchema = cdmDatabaseSchema
+      )
+    )
+    
+    testthat::expect_error(
+      getRecommendedSource(
+        conceptIds = 0,
+        vocabularyDatabaseSchema = cdmDatabaseSchema,
+        connection = connection,
+        conceptPrevalenceSchema = cdmDatabaseSchema
+      )
+    )
+    
+    DatabaseConnector::disconnect(connection = connection)
   }
 })
